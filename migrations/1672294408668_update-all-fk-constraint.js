5cc02cea0f9c0feb4b650c618cd8bf8b4b9a66aa
Zatfley <alfi.dim1612@gmail.com>
2022-12-30 10:39:09 +0700

    fixing update constraint migrate

--- migrations/1672294408668_update-all-fk-constraint.js
+++ migrations/1672294408668_update-all-fk-constraint.js
@@ -4,7 +4,7 @@ exports.shorthands = undefined;
 
 exports.up = (pgm) => {
   // menghapus semua cosntraint fk lama (not on delete cascade)
-  pgm.dropConstraint('songs', 'songs_albumId_fkey');
+  pgm.dropConstraint('songs', 'songs_album_id_fkey');
   pgm.dropConstraint('playlist_song_activities', 'playlist_song_activities_user_id_fkey');
   pgm.dropConstraint('playlist_song_activities', 'playlist_song_activities_song_id_fkey');
   pgm.dropConstraint('playlist_song_activities', 'playlist_song_activities_playlist_id_fkey');
@@ -14,7 +14,7 @@ exports.up = (pgm) => {
   pgm.dropConstraint('playlist_songs', 'playlist_songs_song_id_fkey');
 
   // menambahkan constraint fk baru (on delete cascade)
-  pgm.addConstraint('songs', 'songs_albumId_fkey', 'FOREIGN KEY(album_id) REFERENCES album(id) ON DELETE CASCADE');
+  pgm.addConstraint('songs', 'songs_album_id_fkey', 'FOREIGN KEY(album_id) REFERENCES album(id) ON DELETE CASCADE');
   pgm.addConstraint('playlist_song_activities', 'playlist_song_activities_user_id_fkey', 'FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE');
   pgm.addConstraint('playlist_song_activities', 'playlist_song_activities_song_id_fkey', 'FOREIGN KEY(song_id) REFERENCES songs(id) ON DELETE CASCADE');
   pgm.addConstraint('playlist_song_activities', 'playlist_song_activities_playlist_id_fkey', 'FOREIGN KEY(playlist_id) REFERENCES playlist(id) ON DELETE CASCADE');
